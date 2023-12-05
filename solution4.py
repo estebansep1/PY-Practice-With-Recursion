0f2a4b701031c257862d3adaeb8ff6d335b3507b
Esteban Sepulveda <tebansd@icloud.com>
2023-12-05 15:16:00 -0800

    solutions 3-5 done

--- solution4.py
+++ solution4.py
@@ -1,1 +1,10 @@
 # Write code for algorithm 4 below
+def a_to_b(a, b):
+  if b < 1:
+    print('Invalid input')
+  elif b == 1:
+    return a
+  else:
+    return a * a_to_b(a, b - 1) 
+  
+print(a_to_b(2, 4))
--- solution5.py
+++ solution5.py
@@ -1,1 +1,8 @@
 # Write code for algorithm 5 below
+def palindrome(str):
+    if len(str) == 1 or len(str) == 0:
+        return True
+    else:
+        return (str[0] == str[-1]) and palindrome(str[1: -1])
+    
+print(palindrome('racecar'))
